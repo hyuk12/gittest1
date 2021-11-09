229c41c0c05717bd280baa14e51e6f3ea5f3b0db
hyuk12 <hotman7025@naver.com>
2021-11-10 00:37:24 +0900

    백준알고리즘

--- ex/src/ch01/Main.java
+++ ex/src/ch01/Main.java
@@ -1,18 +1,28 @@
 package ch01;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
-		int a = sc.nextInt();
-		int b = sc.nextInt();
-		while(a > 0 && b >0) {
-			
-			System.out.println(a + b);
-			a = sc.nextInt();
-			b = sc.nextInt();
+		StringTokenizer st;
+		
+		for(int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine()," ");
+			bw.write((Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken())) + "\n");
 		}
+		br.close();
+		bw.flush();
+		bw.close();
+	    
+	    
 	}
 }
